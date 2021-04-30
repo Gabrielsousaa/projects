b3bc56b6905a7f26f03fb355495ff4a3226d63f7
Gabriel de Sousa Souto <gsousax2008@Hotmail.com>
2021-04-29 21:30:47 -0300

    projeto att
    comentarios adicionados sobre as funcçoes

--- jpa_Project/jpaproject/src/main/java/com/jpa/App.java
+++ jpa_Project/jpaproject/src/main/java/com/jpa/App.java
@@ -1,6 +1,6 @@
 package com.jpa;
 
-
+//RESOURCES/META-INF SEMPRE FICA NO MAIN
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -10,15 +10,20 @@ public class App
 {
     public static void main( String[] args )
     {
-        pessoa p1 = new pessoa(null, "gabriel", "gabriel@gmail.com");
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo");
+        pessoa p1 = new pessoa(null, "gabriel", "gabriel@gmail.com");// CRIA UMA PESSOA NO BANCO
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo"); // BUSCA O PERSISTENCE COM ESSE NOME
         EntityManager em = emf.createEntityManager();
-        pessoa p = em.find(pessoa.class, 1);
-        System.out.println(p);
+        //pessoa p = em.find(pessoa.class, 1);//busca uma pessoa no banco
+        //System.out.println(p); 
         //em.getTransaction().begin();// começa a transação para o banco
         //em.persist(p1);// cadastra no banco
         //em.getTransaction().commit();// commita o que foi adicionado
-    
+
+        
+        pessoa p = em.find(pessoa.class, 1);
+        em.getTransaction().begin();// SEMPRE COMECE O GET TRANSACTION ONDE N SEJA UMA SIMPLES CONSULTA
+        em.remove(p);
+        em.getTransaction().commit();//// SEMPRE COMECE O GET TRANSACTION ONDE N SEJA UMA SIMPLES CONSULTA
         System.out.println("DADOS CADASTRADOS");
         em.close();
         emf.close();
